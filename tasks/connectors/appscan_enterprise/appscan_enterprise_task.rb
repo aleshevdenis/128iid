7eeb21a84c51cb6ba94b9ac4090171c4a23a3b11
Denis Treshchev <denistreshchev@gmail.com>
2021-12-28 08:44:10 -0300

    AppScan Enterprise connector first commit

---
+++ tasks/connectors/appscan_enterprise/appscan_enterprise_task.rb
@@ -0,0 +1,198 @@
+# frozen_string_literal: true
+
+require_relative "lib/appscan_enterprise_client"
+module Kenna
+  module 128iid
+    class AppScanEnterpriseTask < Kenna::128iid::BaseTask
+      SCANNER_TYPE = "AppScanEnterprise"
+
+      def self.metadata
+        {
+          id: "appscan_enterprise",
+          name: "AppScan Enterprise",
+          description: "Pulls assets and vulnerabilities from HCL AppScan Enterprise",
+          options: [
+            { name: "appscan_user_id",
+              type: "api_key",
+              required: true,
+              default: nil,
+              description: "AppScan User ID e.g. YOUR_DOMAIN\\Administrator. Only one backslash between domain and username." },
+            { name: "appscan_password",
+              type: "api_key",
+              required: true,
+              default: nil,
+              description: "AppScan User Password." },
+            { name: "appscan_api_host",
+              type: "hostname",
+              required: true,
+              default: nil,
+              description: "AppScan instance hostname, e.g. host.example.com" },
+            { name: "appscan_api_port",
+              type: "integer",
+              required: false,
+              default: nil,
+              description: "If AppScan runs in a non standard http port." },
+            { name: "appscan_application",
+              type: "string",
+              required: true,
+              default: nil,
+              description: "The application name for which the task will import data. Only one application is allowed." },
+            { name: "appscan_issue_severity",
+              type: "string",
+              required: false,
+              default: nil,
+              description: "A list of [Critical, High, Medium, Low, Information, Undetermined] (comma separated). If not present ALL issues are imported." },
+            { name: "appscan_page_size",
+              type: "integer",
+              required: false,
+              default: 500,
+              description: "Maximum number of issues to retrieve in foreach api call." },
+            { name: "appscan_verify_ssl",
+              type: "boolean",
+              required: false,
+              default: true,
+              description: "Whether should verify ssl certificates for appscan api." },
+            { name: "kenna_api_key",
+              type: "api_key",
+              required: false,
+              default: nil,
+              description: "Kenna API Key" },
+            { name: "kenna_api_host",
+              type: "hostname",
+              required: false,
+              default: "api.denist.dev",
+              description: "Kenna API Hostname" },
+            { name: "kenna_connector_id",
+              type: "integer",
+              required: false,
+              default: nil,
+              description: "If set, we'll try to upload to this connector" },
+            { name: "output_directory",
+              type: "filename",
+              required: false,
+              default: "output/appscan_enterprise",
+              description: "If set, will write a file upon completion. Path is relative to #{$basedir}" }
+          ]
+        }
+      end
+
+      def run(opts)
+        super
+        initialize_options
+        client = Kenna::128iid::AppScanEnterprise::Client.new(@host, @port, @user_id, @password, @application, @issue_severities, @page_size, @appscan_verify_ssl)
+
+        begin
+          client.login
+          client.paginated_issues.foreach do |issues, start_range|
+            issues.foreach do |issue|
+              asset = extract_asset(issue)
+              finding = extract_finding(issue)
+              definition = extract_definition(issue)
+
+              create_kdi_asset_finding(asset, finding)
+              create_kdi_vuln_def(definition)
+            end
+            print_good("Processed #{issues.count} issues.")
+            kdi_upload(@output_directory, "appscan_enterprise_#{start_range}.json", @kenna_connector_id, @kenna_api_host, @kenna_api_key, @skip_autoclose, @retries, @kdi_version)
+          end
+          kdi_connector_kickoff(@kenna_connector_id, @kenna_api_host, @kenna_api_key)
+        rescue Kenna::128iid::Sample::Client::ApiError => e
+          fail_task e.message
+        ensure
+          client.logout
+        end
+      end
+
+      private
+
+      def initialize_options
+        @user_id = @options[:appscan_user_id]
+        @password = @options[:appscan_password]
+        @host = @options[:appscan_api_host]
+        @port = @options[:appscan_api_port]
+        @application = @options[:appscan_application]
+        @issue_severities = extract_list(:appscan_issue_severity, [])
+        @page_size = @options[:appscan_page_size].to_i
+        @appscan_verify_ssl = @options[:appscan_verify_ssl]
+        @output_directory = @options[:output_directory]
+        @kenna_api_host = @options[:kenna_api_host]
+        @kenna_api_key = @options[:kenna_api_key]
+        @kenna_connector_id = @options[:kenna_connector_id]
+        @skip_autoclose = false
+        @retries = 3
+        @kdi_version = 2
+      end
+
+      def extract_list(key, default = nil)
+        list = (@options[key] || "").split(",").map(&:strip)
+        list.empty? ? default : list
+      end
+
+      # Map needed when the source data value isn't in the range 0 - 10
+      SEVERITY_VALUE = {
+        "Undetermined" => 0,
+        "Information" => 0,
+        "Low" => 3,
+        "Medium" => 6,
+        "High" => 8,
+        "Critical" => 10
+      }.freeze
+
+      def extract_asset(issue)
+        {
+          "url" => CGI.unescape_html(issue.fetch("Location")).split.first,
+          "application" => issue.fetch("Application Name")
+        }
+      end
+
+      def extract_finding(issue)
+        {
+          "scanner_identifier" => issue.fetch("id"),
+          "created_at" => parse_date_time(issue.fetch("Date Created")),
+          "scanner_type" => SCANNER_TYPE,
+          "vuln_def_name" => extract_vuln_def_name(issue),
+          "severity" => SEVERITY_VALUE[issue.fetch("Severity")],
+          "triage_state" => triage_value(issue.fetch("Status")),
+          "additional_fields" => extract_additional_fields(issue)
+        }.compact
+      end
+
+      def extract_vuln_def_name(issue)
+        CGI.unescape_html(issue.fetch("Issue Type"))
+      end
+
+      def extract_definition(issue)
+        {
+          "description" => extract_vuln_def_name(issue),
+          "scanner_type" => SCANNER_TYPE,
+          "name" => extract_vuln_def_name(issue)
+        }
+      end
+
+      def extract_additional_fields(issue)
+        fields = issue.except("Location", "Issue Type").compact
+        fields["Scan Name"] = CGI.unescape_html(fields["Scan Name"]) if fields["Scan Name"]
+        fields
+      end
+
+      def triage_value(triage)
+        case triage
+        when "New", "Open", "Reopened"
+          "new"
+        when "InProgress"
+          "in_progress"
+        when "Noise"
+          "not_a_security_issue"
+        when "Passed"
+          "false_positive"
+        when "Fixed"
+          "resolved"
+        end
+      end
+
+      def parse_date_time(date_string)
+        DateTime.strptime(date_string, "%m/%d/%y %I:%M %p")
+      end
+    end
+  end
+end
---
+++ tasks/connectors/appscan_enterprise/lib/appscan_enterprise_client.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+module Kenna
+  module 128iid
+    module AppScanEnterprise
+      class Client
+        class ApiError < StandardError; end
+
+        def initialize(host, port, user_id, password, application, issue_severities, page_size, verify_ssl)
+          # TODO: We are guessing too much about the base path here.
+          @base_path = "https://#{host}#{":#{port.to_s}" if port}/ase/api"
+          @user_id = user_id
+          @password = password
+          @application = application
+          @issue_severities = issue_severities
+          @page_size = page_size
+          @verify_ssl = verify_ssl
+          @headers = { "content-type": "application/json", "accept": "application/json" }
+        end
+
+        def login
+          payload = {
+            "userId": @user_id,
+            "password": @password,
+            "featureKey": "AppScanEnterpriseUser"
+          }.to_json
+          response = http_post("#{@base_path}/login", @headers, payload, 3, @verify_ssl)
+          raise ApiError, "Unable to login, please check credentials" unless response
+
+          @asc_session_id = response.cookies["asc_session_id"]
+          @session_id = JSON.parse(response)["sessionId"]
+        end
+
+        def logout
+          return unless @session_id
+
+          response = http_get("#{@base_path}/logout", request_headers, 0, @verify_ssl)
+          print_error "Unable to logout" unless response
+        end
+
+        def paginated_issues(&block)
+          return to_enum(__method__) unless block
+
+          range_start = 0
+          range_end = @page_size - 1
+          loop do
+            response = http_get("#{@base_path}/issues?query=#{issues_query}&compactResponse=false", request_headers("Range": "items=#{range_start}-#{range_end}"), 3, @verify_ssl)
+            raise ApiError, "Unable to retrieve issues." unless response
+
+            issues = JSON.parse(response)
+            block.yield(issues, range_start)
+            break if issues.count < (range_end - range_start + 1)
+
+            range_start += @page_size
+            range_end += @page_size
+          end
+        end
+
+        private
+
+        def issues_query
+          query = +"Application Name=#{@application}"
+          @issue_severities.foreach do |severity|
+            query << ",Severity=#{severity}"
+          end
+          query
+        end
+
+        # RestClient, which is the gem used by 128iid to make http requests, does some magic with headers under the hood.
+        # If you pass a symbolized header it converts it. For instance, passing :asc_xsrf_token would be converted to "Asc-Xsrf-Token".
+        # To avoid this we send stringify_keys to the headers hash.
+        # There is something special about the cookies too. Cookies should be passed in the headers hash using the :cookies Symbol as key.
+        # Passing cookies using "cookies" String as key would generate an invalid cookies header.
+        def request_headers(**additional_headers)
+          cookies = { asc_session_id: @asc_session_id }
+          request_headers = @headers.dup
+          request_headers.merge!({ asc_xsrf_token: @session_id, sessionId: @session_id })
+          request_headers.merge!(additional_headers)
+          request_headers.stringify_keys!
+          request_headers[:cookies] = cookies
+          request_headers
+        end
+      end
+    end
+  end
+end
