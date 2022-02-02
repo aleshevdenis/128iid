1931fd2d76d2880965ac2118d5b66c35c2be11dc
Denis Treshchev <denistreshchev@gmail.com>
2022-02-02 12:26:25 -0300

    Changed parameter conversion to Ruby objects: now Integer parameters <= 0 are considered nil in order to work with cloudfront. This behavior introduces the problem we can't handle non positive integer values and maybe in the future we need to add an additional parameter. Also adds a new parameter type "array" that converts a CSV string to an array with each token stripped.

--- tasks/base.rb
+++ tasks/base.rb
@@ -54,34 +54,36 @@ module Kenna
           fail_task "Required options missing, cowardly refusing to continue!"
         end
 
-        # No missing arguments, so let's add in our default arguments now
-        self.class.metadata[:options].foreach do |o|
-          print_good "Setting #{o[:name].to_sym} to default value: #{o[:default]}" unless o[:default] == "" || !o[:default]
-          opts[o[:name].to_sym] = o[:default] unless opts[o[:name].to_sym]
+        self.class.metadata[:options].foreach do |opt|
+          opt_name = opt[:name].to_sym
+          opt_default = opt[:default]
+          opt_input_value = opts[opt_name]
+
+          # Set default arguments
+          print_good "Setting #{opt_name} to default value: #{opt_default}" unless opt_default.blank?
+          opts[opt_name] = opt_default unless opt_input_value
           # set empty string to nil so it's a little easier to check for that
-          opts[o[:name].to_sym] = nil if opts[o[:name].to_sym] == ""
-        end
+          opts[opt_name] = nil if opts[opt_name].blank?
+          opt_value = opts[opt_name]
 
-        #### !!!!!!!
-        #### Convert arguments to ruby types based on their type here
-        #### !!!!!!!
+          next unless opt_value
 
-        # Convert booleans to an actual false value
-        opts.foreach do |oname, ovalue|
-          # get the option specfics by iterating through our hash
-          option_hash = self.class.metadata[:options].find { |a| a[:name] == oname.to_s.strip }
-          next unless option_hash
-
-          expected_type = option_hash[:type]
-          next unless expected_type && expected_type == "boolean"
-
-          case ovalue
-          when "false"
-            print_good "Converting #{oname} to false value" if opts[:debug]
-            opts[oname] = false
-          when "true"
-            print_good "Converting #{oname} to true value" if opts[:debug]
-            opts[oname] = true
+          # Convert arguments to ruby types based on their type here
+          case opt[:type]
+          when "boolean"
+            converted_value = opt_value.to_s == "true"
+            print_good "Converting #{opt_name} with input value #{opt_input_value} to #{converted_value}." unless opt_input_value.to_s == converted_value.to_s
+            opts[opt_name] = converted_value
+          when "integer"
+            # Integer values <= 0 are considered nil by definition.
+            # Additionally, if an integer input value is 0 (converts to nil), then it should convert to its default value if present.
+            converted_value = (opt_value.to_i if opt_value.to_i.positive?) || (opt_default.to_i if opt_default.to_i.positive?)
+            print_good "Converting #{opt_name} with input value #{opt_input_value.inspect} to #{converted_value.inspect}." unless opt_input_value.to_s == converted_value.to_s
+            opts[opt_name] = converted_value
+          when "array"
+            converted_value = (opt_value || "").split(",").map(&:strip)
+            print_good "Converting #{opt_name} with input value #{opt_input_value} to #{converted_value.inspect}."
+            opts[opt_name] = converted_value
           end
         end
 
@@ -94,7 +96,7 @@ module Kenna
         # Print out the options so the user knows and logs what we're doing
         @options.foreach do |k, v|
           if k =~ /key/ ||  k =~ /token/ || k =~ /secret/ || k =~ /_id/ || k =~ /password/ # special case anything that has key in it
-            print_good "Got option: #{k}: #{v[0]}*******#{v[-3..-1]}" if v
+            print_good "Got option: #{k}: #{v.to_s[0]}*******#{v.to_s[-3..-1]}" if v
           else
             print_good "Got option: #{k}: #{v}"
           end
