00b31dd34f81e5fc45d27c4a1daef2829a814a55
Denis Treshchev <denistreshchev@gmail.com>
2022-08-17 14:52:06 -0500

    Change params to correct RestClient format, remove api check with bad endpoint

--- tasks/connectors/ntt_sentinel_dynamic/lib/api_client.rb
+++ tasks/connectors/ntt_sentinel_dynamic/lib/api_client.rb
@@ -77,8 +77,8 @@ module Kenna
           retries = options.delete(:retries) { |_k| 5 }
 
           url = "#{BASE_PATH}#{path}"
-          params = { key: @api_key, format: :json }.merge(options)
-          response = Kenna::128iid::Helpers::Http.http_get(url, { params: }, retries)
+          params = { key: @api_key, accept: 'application/json' }.merge({params: options})
+          response = Kenna::128iid::Helpers::Http.http_get(url, params, retries)
 
           raise Error unless response
 
