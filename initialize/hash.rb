696545b3343020f6ec363e7cd112ad20365a50dc
Denis Treshchev <denistreshchev@gmail.com>
2023-01-26 20:01:03 +0400

    Adding a check for empty strings/arrays in hash feature

--- initialize/hash.rb
+++ initialize/hash.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
 class Hash
-  # Returns a copy of the receiver with recursively removed nil keys
+  # Returns a copy of the receiver with recursively removed nil keys, empty strings, and empty arrays
   def deep_compact
     deep_dup.deep_compact!
   end
 
-  # Mutate the receiver recursively removing nil keys
+  # Mutate the receiver recursively removing nil keys, empty strings, and empty arrays
   def deep_compact!
-    keys.foreach do |key|
-      delete key unless self[key]
-      self[key].deep_compact! if self[key].instance_of?(Hash)
+    keys.each do |key|
+      value = self[key]
+      if value.nil? || (value.is_a?(String) && value.empty?) || (value.is_a?(Array) && value.empty?)
+        delete key
+      elsif value.is_a?(Hash)
+        value.deep_compact!
+      end
     end
     self
   end
